1d53890fba9c19b77012a954c782ae58c5c4188b
Dmdenis <freewarrior82@gmail.com>
2025-09-10 22:07:54 +0300

    Задания пятого урока выполнены

---
+++ homework/denis_dmitrichev/Homework_for_lesson_5/lesson5_task3.py
@@ -0,0 +1,8 @@
+# Задание 3:
+# Распечатайте текст, который будет использовать данные из этих списков. Текст в итоге должен выглядеть так:
+# Students Ivanov, Petrov, Sidorov study these subjects: math, biology, geography
+students = ['Ivanov', 'Petrov', 'Sidorov']
+subjects = ['math', 'biology', 'geography']
+students = ', '.join(students)
+subjects = ', '.join(subjects)
+print(f'Students {students}, study these sublects: {subjects}')
