dfa5b8e2664850930c52a943d7b91e90abbede7a
Dmdenis <freewarrior82@gmail.com>
2025-09-18 22:33:53 +0300

    Вместо трёх принтов сделал один с разделителем \n

--- homework/denis_dmitrichev/Homework_for_lesson_9/lesson9_task2.py
+++ homework/denis_dmitrichev/Homework_for_lesson_9/lesson9_task2.py
@@ -4,6 +4,6 @@
 temperatures = [20, 15, 32, 34, 21, 19, 25, 27, 30, 32, 34, 30, 29, 25, 27,
                 22, 22, 23, 25, 29, 29, 31, 33, 31, 30, 32, 30, 28, 24, 23]
 hot_temperatures = list(filter(lambda x: x > 28, temperatures))
-print(f'Максимальная температура: {max(hot_temperatures)}')
-print(f'Минимальная температура: {min(hot_temperatures)}')
-print(f'Средняя температура: {round(sum(hot_temperatures) / len(hot_temperatures), 2)}')
+print(f'Максимальная температура: {max(hot_temperatures)}',
+      f'Минимальная температура: {min(hot_temperatures)}',
+      f'Средняя температура: {round(sum(hot_temperatures) / len(hot_temperatures), 2)}', sep='\n')
